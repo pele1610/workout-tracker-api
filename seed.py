a790431c93fc945d06acac3d84b0019adf033b78
pele1610 <pelecolo56@gmail.com>
2026-07-26 14:32:39 +0300

    Add seed file: link exercises to workouts via WorkoutExercise

--- seed.py
+++ seed.py
@@ -29,4 +29,15 @@ with app.app_context():
     db.session.add_all([workout_1, workout_2])
     db.session.commit()
 
-    print("Done!")
+    print("Linking exercises to workouts...")
+    we1 = WorkoutExercise(workout_id=workout_1.id, exercise_id=pushups.id, reps=15, sets=3, duration_seconds=None)
+    we2 = WorkoutExercise(workout_id=workout_1.id, exercise_id=squats.id, reps=12, sets=3, duration_seconds=None)
+    we3 = WorkoutExercise(workout_id=workout_1.id, exercise_id=bench_press.id, reps=8, sets=4, duration_seconds=None)
+    we4 = WorkoutExercise(workout_id=workout_2.id, exercise_id=running.id, reps=None, sets=None, duration_seconds=1200)
+
+    db.session.add_all([we1, we2, we3, we4])
+    db.session.commit()
+
+    print("Done seeding!")
+
+    
