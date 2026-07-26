d703baa4e31cb456c3857bf75bd36f65371d1ff2
pele1610 <pelecolo56@gmail.com>
2026-07-26 13:46:27 +0300

    Add Exercise model

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+
+
